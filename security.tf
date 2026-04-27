40d6fbe0297646a6cc25f8ce77ec8f0f56e5fb4f
CodeKeeper <codekeeper@amazon.com>
2026-04-27 15:02:28 +0000

    CodeKeeper: IaC modification

--- security.tf
+++ security.tf
@@ -66,7 +66,7 @@ resource "aws_security_group" "bastion" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = var.bastion_allowed_cidrs
   }
 
   egress {
