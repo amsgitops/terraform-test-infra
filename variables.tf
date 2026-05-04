58a912b754c64f50b9dcd5947232d643140f6b2a
CodeKeeper <codekeeper@amazon.com>
2026-05-04 13:38:15 +0000

    CodeKeeper: IaC modification

--- variables.tf
+++ variables.tf
@@ -15,19 +15,19 @@ variable "project_name" {
 
 variable "vpc_cidr" {
   description = "VPC CIDR block"
-  default     = "10.0.0.0/16"
+  default     = "172.16.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
   description = "Public subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["172.16.1.0/24", "172.16.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
   description = "Private subnet CIDR blocks"
   type        = list(string)
-  default     = ["10.0.10.0/24", "10.0.11.0/24"]
+  default     = ["172.16.10.0/24", "172.16.11.0/24"]
 }
 
 variable "enable_nat_gateway" {
@@ -54,4 +54,22 @@ variable "db_name" {
 variable "alarm_email" {
   description = "Email for CloudWatch alarm notifications"
   default     = "alerts@example.com"
+}
+
+variable "bastion_allowed_cidrs" {
+  description = "CIDR blocks permitted to SSH into the bastion host"
+  type        = list(string)
+
+  validation {
+    condition = alltrue([
+      for cidr in var.bastion_allowed_cidrs :
+      cidr != "0.0.0.0/0" && cidr != "::/0"
+    ])
+    error_message = "bastion_allowed_cidrs must not contain 0.0.0.0/0 or ::/0. Restrict SSH to known operator CIDRs."
+  }
+}
+
+variable "bastion_key_name" {
+  description = "EC2 key pair name for bastion SSH access"
+  type        = string
 }
