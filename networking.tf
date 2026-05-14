4c8c832812376da8c07e65d6220237561a976284
CodeKeeper <codekeeper@amazon.com>
2026-05-14 04:51:20 +0000

    CodeKeeper: IaC modification

--- networking.tf
+++ networking.tf
@@ -19,10 +19,14 @@ resource "aws_subnet" "public" {
   availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
 
-  tags = {
-    Name = "${var.project_name}-public-${count.index + 1}"
-    Tier = "public"
-  }
+  tags = merge(
+    {
+      Name = "${var.project_name}-public-${count.index + 1}"
+      Tier = "public"
+    },
+    # LoadTestTag targets subnet-03c0df3cf0cfdfca8 (index 0)
+    count.index == 0 ? { LoadTestTag = "1778734205234073352" } : {}
+  )
 }
 
 resource "aws_subnet" "private" {
