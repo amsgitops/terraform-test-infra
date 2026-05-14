8427440f62934bf4f85b68e6f6d22fb5a80af971
CodeKeeper <codekeeper@amazon.com>
2026-05-14 01:11:48 +0000

    CodeKeeper: IaC modification

--- variables.tf
+++ variables.tf
@@ -59,5 +59,5 @@ variable "alarm_email" {
 variable "sns_display_name" {
   description = "Display name for the SNS alerts topic (visible in email/SMS subjects)"
   type        = string
-  default     = "CodeKeeper E2E 1778720980"
+  default     = "UpdateTest 1778721072"
 }
