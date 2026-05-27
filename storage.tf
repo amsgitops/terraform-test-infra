14e6f3c16d96c7a0324cc2c0910920dc336eae0c
CodeKeeper <codekeeper@amazon.com>
2026-05-27 04:38:38 +0000

    CodeKeeper: IaC modification

--- storage.tf
+++ storage.tf
@@ -31,3 +31,35 @@ resource "aws_s3_bucket_public_access_block" "app_data" {
 }
 
 data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "codekeeper_eu_test_bucket_001" {
+  bucket = "codekeeper-eu-test-bucket-001-${data.aws_caller_identity.current.account_id}"
+
+  tags = {
+    Name = "codekeeper-eu-test-bucket-001"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "codekeeper_eu_test_bucket_001" {
+  bucket = aws_s3_bucket.codekeeper_eu_test_bucket_001.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "codekeeper_eu_test_bucket_001" {
+  bucket = aws_s3_bucket.codekeeper_eu_test_bucket_001.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "codekeeper_eu_test_bucket_001" {
+  bucket                  = aws_s3_bucket.codekeeper_eu_test_bucket_001.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
