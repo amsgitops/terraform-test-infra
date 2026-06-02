169d05f402121eee6de43c5f555100763cafcac6
CodeKeeper <codekeeper@amazon.com>
2026-06-02 06:06:00 +0000

    CodeKeeper: IaC modification

--- monitoring.tf
+++ monitoring.tf
@@ -1,9 +1,9 @@
 resource "aws_sns_topic" "alerts" {
-  name         = "${var.project_name}-alerts"
+  name         = "codekeeper-test-alerts-e2e-t12"
   display_name = var.sns_display_name
 
   tags = {
-    Name        = "${var.project_name}-alerts"
+    Name        = "codekeeper-test-alerts-e2e-t12"
     Environment = var.environment
   }
 }
